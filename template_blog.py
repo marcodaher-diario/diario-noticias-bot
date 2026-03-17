7e3257c7a4ccf196498261442d38b17a6f3cc8d0
marcodaher-diario <marco.daher@gmail.com>
2026-03-17 01:37:16 -0300

    Update template_blog.py

--- template_blog.py
+++ template_blog.py
@@ -12,16 +12,16 @@ def formatar_texto(texto, titulo_principal):
 
     lista_aberta = False
 
-    for linha in linhas:
+    for i, linha in enumerate(linhas):
 
         linha_limpa = linha.strip()
-
+    
         # converter **negrito**
-        linha_limpa = re.sub(r"\*\*(.*?)\*\*", r"<strong>\1</strong>", linha_limpa)
-
+        linha_limpa = re.sub(r"\*\*([^\*]+)\*\*", r"<strong>\1</strong>", linha_limpa)
+    
         # remove markdown
         linha_limpa = linha_limpa.strip("# ").strip()
-
+    
         # remove repetição do título
         if linha_limpa.lower() == titulo_norm:
             continue
@@ -51,10 +51,13 @@ def formatar_texto(texto, titulo_principal):
         # DETECÇÃO DE SUBTÍTULO (H2)
         # =========================
         condicao_subtitulo = (
-            3 <= len(palavras) <= 22
-            and not linha_limpa.endswith(".")
-            and not linha_limpa.endswith(":")
-            and linha_limpa[0].isupper()
+            (i == 0 and 3 <= len(palavras) <= 22)
+            or (
+                3 <= len(palavras) <= 22
+                and not linha_limpa.endswith(".")
+                and not linha_limpa.endswith(":")
+                and linha_limpa[0].isupper()
+            )
         )
 
         if condicao_subtitulo:
